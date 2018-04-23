ac16b80dc4895ff8a2df8188014b2ffbc39e5295
Angelica Pelizza <aangelicap@users.noreply.github.com>
2018-04-23 16:22:36 -0300

    Update arduino.ino

--- arduino.ino
+++ arduino.ino
@@ -11,49 +11,35 @@ void setup(void) {
 
 void loop(void) {
   fsrReading = analogRead(fsrPin);
-  //Serial.print("Analog reading = ");
-  //Serial.println(fsrReading);
-
-  // analog voltage reading ranges from about 0 to 1023 which maps to 0V to 5V (= 5000mV)
-  //fsrVoltage = map(fsrReading, 0, 1023, 0, 5000);
-    fsrVoltage = fsrReading*(5000/1023);
-  //Serial.print("Voltage reading in mV = ");
-  //Serial.println(fsrVoltage);
+  fsrVoltage = fsrReading*(5000/1023);
 
   if (fsrVoltage == 0) {
     Serial.print("I0");
     Serial.print("\n");
     delay(20);
   } else {
-    // The voltage = Vcc * R / (R + FSR) where R = 10K and Vcc = 5V
-    // so FSR = ((Vcc - V) * R) / V        yay math!
     fsrResistance = 5000 - fsrVoltage;     // fsrVoltage is in millivolts so 5V = 5000mV
     fsrResistance *= 10000;                // 10K resistor
     fsrResistance /= fsrVoltage;
-    //Serial.print("FSR resistance in ohms = ");
-    //Serial.println(fsrResistance);
 
     fsrConductance = 1000000;           // we measure in micromhos so
     fsrConductance /= fsrResistance;
-    //Serial.print("Conductance in microMhos: ");
-    //Serial.println(fsrConductance);
-
-    // Use the two FSR guide graphs to approximate the force
-    if (fsrConductance <= 1000) {
-      fsrForce = fsrConductance / 80;
-      Serial.print("I");
-      Serial.print(fsrForce);
-      Serial.print("\n");
-      delay(20);
-    } else {
-      fsrForce = fsrConductance - 1000;
-      fsrForce /= 30;
-      Serial.print("I");
-      Serial.print(fsrForce);
-      Serial.print("\n");
-      delay(20);
-    }
+    
+      // Use the two FSR guide graphs to approximate the force
+      if (fsrConductance <= 1000) {
+        fsrForce = fsrConductance / 80;
+        Serial.print("I");
+        Serial.print(fsrForce);
+        Serial.print("\n");
+        delay(20);
+       } else {
+        fsrForce = fsrConductance - 1000;
+        fsrForce /= 30;
+        Serial.print("I");
+        Serial.print(fsrForce);
+        Serial.print("\n");
+        delay(20);
+      }
   }
-  //Serial.println("--------------------");
   delay(1000);
 }
