08a0f25bffdad829b1d52b53530586855877f76d
Angelica Pelizza <aangelicap@users.noreply.github.com>
2018-04-23 16:15:49 -0300

    Update circularList.h

--- circularList.h
+++ circularList.h
@@ -3,6 +3,10 @@ struct record {
  struct timeval * timestamp;
 };
 
+/**
+*Função utilizada em serverProgram.c e client.c para manipulação de dados da lista circular
+* e também em channelClient.c na função listenServer. 
+*/
 void insertRecord(char data, struct timeval * timestamp);
 
 struct record * readRecord(struct record * recordValue);
