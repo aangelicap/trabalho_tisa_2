4d4b78421c560135c3e78e24cbfeb6c4fa97f6b8
Angelica Pelizza <aangelicap@users.noreply.github.com>
2018-04-23 16:25:12 -0300

    Update bufferControl.h

--- bufferControl.h
+++ bufferControl.h
@@ -4,6 +4,10 @@ struct buffer_data
     struct timeval * timestamp;
 };
 
+/**
+* Esta função é utilizada em client.c e em serverProgram.c pela função logInfo
+* que é responsável por realizar a gravação nos arquivos de log: serverLog.txt e clientLog.txt 
+*/
 void insertValue(char data, struct timeval * timestamp);
 
 struct buffer_data * waitFullBuffer (void);
